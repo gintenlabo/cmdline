1a4ca1bbbf67e1cab51c7d66f7dd66c8d0ec7701
Takaya SAITO (SubaruG) <gintensubaru@gmail.com>
2017-12-20 17:29:30 +0900

    Modify demangle for MSVC

--- cmdline.h
+++ cmdline.h
@@ -36,8 +36,11 @@
 #include <typeinfo>
 #include <cstring>
 #include <algorithm>
-#include <cxxabi.h>
 #include <cstdlib>
+
+#ifdef __GNUC__
+#include <cxxabi.h>
+#endif
 
 namespace cmdline{
 
@@ -104,11 +107,17 @@ Target lexical_cast(const Source &arg)
 
 static inline std::string demangle(const std::string &name)
 {
+#ifdef __GNUC__
   int status=0;
   char *p=abi::__cxa_demangle(name.c_str(), 0, 0, &status);
   std::string ret(p);
   free(p);
   return ret;
+#elif defined(_MSC_VER)
+  return name;
+#else
+#error Unexpected C++ complier; Implement this method to demangle
+#endif
 }
 
 template <class T>
